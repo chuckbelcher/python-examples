05e6267e10d9f89caffc7c6ac78d82a99222f23f
Chuck Belcher <cfbelcher@pateogroup.com>
2021-10-07 21:57:34 -0400

    iinitial commit

---
+++ datatypes.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+# Data Types: int, float, str, lst, dict, tup, set, bool
+# Basic Math
+print(2+1)
+print(2-1)
+print(2*1)
+print(12/5)
+print(12 % 5)  # Just get the remainder
+print(2 ** 5)  # 2 to the power of 5
+print(2+10*10+3 - (10*5))  # Order of operation example results in 105-50
+
+numb_girls = 2
+numb_boys = 2
+numb_girls = numb_girls + 2  # added the grandchildren
+numb_boys = numb_boys + 1  # added the grandchildren
+numb_kids = numb_girls + numb_boys
+
+print(numb_kids)
+print(type(numb_kids))
+
+# Stringing things along
+x = "Hello World"
+print(x.upper())
+print(x.split())  # split by a white space
+print(x.split('o'))  # split by the letter o
---
+++ readme.md
@@ -0,0 +1,5 @@
+##Create virtual environment
+- /usr/local/bin/python3 -m venv ./virtp3
+- source ./virt_python3/bin/activate
+
+
